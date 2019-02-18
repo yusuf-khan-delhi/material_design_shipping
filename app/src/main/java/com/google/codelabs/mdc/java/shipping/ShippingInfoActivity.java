062c8e56821c7f220941e4241bf789a3990ea68c
yusuf.vicky <yusufkhan1985@gmail.com>
2019-02-18 13:20:00 +0530

    edit text focus issue removed

--- app/src/main/java/com/google/codelabs/mdc/java/shipping/ShippingInfoActivity.java
+++ app/src/main/java/com/google/codelabs/mdc/java/shipping/ShippingInfoActivity.java
@@ -6,12 +6,15 @@ import android.graphics.Color;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -23,18 +26,21 @@ import java.util.List;
 public class ShippingInfoActivity extends AppCompatActivity {
 
     private String selectedItemText;
+    private LinearLayout parent_layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.shipping_info_activity);
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
 
         View rootView = findViewById(android.R.id.content);
+        Button button = findViewById(R.id.save_button);
+        parent_layout = findViewById(R.id.parent_layout);
 
         final List<TextInputLayout> textInputLayouts = Utils.findViewsWithType(
                 rootView, TextInputLayout.class);
 
-        Button button = findViewById(R.id.save_button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -50,6 +56,7 @@ public class ShippingInfoActivity extends AppCompatActivity {
                 }
                 if (noErrors) {
                     // All fields are valid!
+
                 }*/
             }
         });
@@ -60,6 +67,12 @@ public class ShippingInfoActivity extends AppCompatActivity {
         final Spinner spinner3 = findViewById(R.id.spinner3);
         final Spinner spinner4 = findViewById(R.id.spinner4);
         final Spinner spinner5 = findViewById(R.id.spinner5);
+
+        onSpnTouchListener(spinner1);
+        onSpnTouchListener(spinner2);
+        onSpnTouchListener(spinner3);
+        onSpnTouchListener(spinner4);
+        onSpnTouchListener(spinner5);
 
         String[] testArray = getResources().getStringArray(R.array.test);
         final List<String> plantsList = new ArrayList<>(Arrays.asList(testArray));
@@ -76,10 +89,29 @@ public class ShippingInfoActivity extends AppCompatActivity {
         onItemSelectedListener(spinner4);
         onItemSelectedListener(spinner5);
 
-        hideKeyboard(this);
+        Utils.hideKeyboard(this);
     }
 
-    public String onItemSelectedListener(Spinner spinner) {
+    private void onSpnTouchListener(Spinner spinner) {
+
+        spinner.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                parent_layout.requestFocus();
+                return false;
+            }
+        });
+
+
+    }
+
+    private void getFocusOnSpinner(Spinner spinner) {
+        spinner.setFocusable(true);
+        spinner.setFocusableInTouchMode(true);
+        spinner.requestFocus();
+    }
+
+    public String onItemSelectedListener(final Spinner spinner) {
 
         selectedItemText = "";
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
@@ -88,6 +120,7 @@ public class ShippingInfoActivity extends AppCompatActivity {
                 selectedItemText = (String) parent.getItemAtPosition(position);
 
                 if (position > 0) {
+
                     Toast.makeText
                             (getApplicationContext(), "Selected : " + selectedItemText, Toast.LENGTH_SHORT)
                             .show();
@@ -95,6 +128,8 @@ public class ShippingInfoActivity extends AppCompatActivity {
                     ((TextView) parent.getChildAt(0)).setTextColor(getResources().getColor(R.color.colorHint));
                 }
             }
+
+
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
@@ -110,17 +145,6 @@ public class ShippingInfoActivity extends AppCompatActivity {
         spinnerArrayAdapter.setDropDownViewResource(R.layout.spinner_item_drop_down);
 
         return spinnerArrayAdapter;
-    }
-
-    public static void hideKeyboard(Activity activity) {
-        InputMethodManager imm = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
-        //Find the currently focused view, so we can grab the correct window token from it.
-        View view = activity.getCurrentFocus();
-        //If no view currently has focus, create a new one, just so we can grab a window token from it
-        if (view == null) {
-            view = new View(activity);
-        }
-        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 
 }
--- app/src/main/java/com/google/codelabs/mdc/java/shipping/Utils.java
+++ app/src/main/java/com/google/codelabs/mdc/java/shipping/Utils.java
@@ -1,7 +1,9 @@
 package com.google.codelabs.mdc.java.shipping;
 
+import android.app.Activity;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,5 +30,16 @@ public abstract class Utils {
                 findViewsWithType(viewGroup.getChildAt(i), type, views);
             }
         }
+    }
+
+    public static void hideKeyboard(Activity activity) {
+        InputMethodManager imm = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        //Find the currently focused view, so we can grab the correct window token from it.
+        View view = activity.getCurrentFocus();
+        //If no view currently has focus, create a new one, just so we can grab a window token from it
+        if (view == null) {
+            view = new View(activity);
+        }
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 }
